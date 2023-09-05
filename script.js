c2ab2ba91b874c0f477e789e52f043097fb5a5f4
bruic87 <bruic@outlook.com>
2023-09-05 20:43:40 -0300

    'aula 3: add de audio e adicao/exclusao de classe'

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('app__card-button--curto')
 const longoBt = document.querySelector('app__card-button--longo')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+}) 
 
 function foco() {    
     alterarContexto('foco')
+    focoBt.classList.add('active')
 }
 
 function curto() {    
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 }
 
 function longo() {    
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 }
 
+
 function alterarContexto (contexto) {
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('acitve')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -25,17 +44,16 @@ function alterarContexto (contexto) {
             titulo.innerHTML = `Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
             break;
-
+            
         case 'descanso-curto':
             titulo.innerHTML = `Que tal dar uma respirada?<br> 
             <strong class="app__title-strong">Faça uma pausa curta!</strong>`
             break;
-
+                
         case 'descanso-longo':
             titulo.innerHTML = `Hora de voltar à superfície.<br>
             <strong class="app__title-strong">Faça uma pausa longa.</strong>`
-
-
+                    
         default:
             break;
     }
